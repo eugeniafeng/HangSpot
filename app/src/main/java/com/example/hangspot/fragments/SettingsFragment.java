f710e6d25cdd65b2d6f32705e7a0655ba844c1f8
eugeniafeng <eugeniafeng@gmail.com>
2021-08-05 01:57:42 -0400

    Added more options to the Settings fragment

--- app/src/main/java/com/example/hangspot/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/hangspot/fragments/SettingsFragment.java
@@ -3,42 +3,25 @@ package com.example.hangspot.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
+import com.example.hangspot.R;
 import com.example.hangspot.activities.AuthenticationActivity;
-import com.example.hangspot.databinding.FragmentSettingsBinding;
 import com.parse.ParseUser;
 
-public class SettingsFragment extends Fragment {
-
-    private FragmentSettingsBinding binding;
-
-    public SettingsFragment() {}
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        binding = FragmentSettingsBinding.inflate(inflater, container, false);
-        // Inflate the layout for this fragment
-        return binding.getRoot();
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        binding.btnLogout.setOnClickListener(v -> {
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        Preference logoutPreference = findPreference("logout");
+        logoutPreference.setOnPreferenceClickListener(preference -> {
             ParseUser.logOutInBackground();
-            // Return to sign up/log in and dismiss the main activity so user cannot use back button
             Intent i = new Intent(getContext(), AuthenticationActivity.class);
             startActivity(i);
             getActivity().finish();
+            return true;
         });
     }
 }
